0aa165551b98ba21bed26a1c507cdfc02e9570b7
Latoya Armstrong <armstronglatoya2505@gmail.com>
2022-11-27 20:03:31 -0300

    updates

---
+++ index.js
@@ -0,0 +1,79 @@
+const form = document.getElementById('form')
+const username = document.getElementById('username')
+const email = document.getElementById('email')
+const password = document.getElementById('password')
+const passwordtwo = document.getElementById('password-two')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    checkInputs()
+})
+
+function checkInputs() {
+
+    const usernameValue = username.value.trim()
+    const emailValue = email.value.trim()
+    const passwordValue = password.value.trim()
+    const passwordtwoValue = passwordtwo.value.trim()
+
+    if(usernameValue === '') {
+        // mostrar erro
+        // add classe
+        setErrorFor(username, 'Preencha esse campo')
+    } else {
+        // adicionar a classe de sucesso
+        setSuccessFor(username)
+    }
+
+    if(emailValue === '') {
+        // mostrar erro
+        // add classe
+        setErrorFor(email, 'Preencha esse campo')
+    } else if ((emailValue)!== emailValue) { 
+        setErrorFor(email, 'Email inválido')
+    } else {
+        // adicionar a classe de sucesso
+        setSuccessFor(email)
+    }
+   
+    if(passwordValue === '') {
+        // mostrar erro
+        // add classe
+        setErrorFor(password, 'Preencha esse campo')
+
+    } else if(passwordValue.length < 8) { 
+        setErrorFor(password, 'Senha deve ter mais que 8 caracteres')
+    } else {
+        // adicionar a classe de sucesso
+        setSuccessFor(password)
+    }
+
+    if(passwordtwoValue === '') {
+        // mostrar erro
+        // add classe
+        setErrorFor(passwordtwo, 'Preencha esse campo')
+
+    } else if(passwordValue !== passwordtwoValue) { 
+        setErrorFor(passwordtwo, 'Senhas são diferentes')
+    } else {
+        // adicionar a classe de sucesso
+        setSuccessFor(passwordtwo)
+    }
+
+}
+
+function setErrorFor(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small')
+
+    small.innerText = message
+
+    formControl.className = 'form-control error'
+}
+
+function setSuccessFor(input) {
+    const formControl = input.parentElement;
+
+    formControl.className = 'form-control success'
+}
